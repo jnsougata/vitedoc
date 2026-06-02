0d547653db96056e6e22d4d2dc2d8612b697bc92
jnsougata <jnsougata@gmail.com>
2026-06-02 17:14:24 +0530

    Add metadata to __init__.py including title, license, author, and version

--- vitedoc/__init__.py
+++ vitedoc/__init__.py
@@ -1,8 +1,14 @@
 """
 vitedoc
-
+---
 An automatic documentation generator for vitepress.
 """
 
+__title__ = "vitedoc"
+__license__ = "MIT"
+__copyright__ = "Copyright 2026-present Sougata Jana"
+__author__ = "Sougata Jana"
+__version__ = "0.1.dev0"
+__url__ = "https://github.com/jnsougata/vitedoc"
 
 from .client import  init
